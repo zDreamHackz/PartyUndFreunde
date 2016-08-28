c6931ddb7db727605fba6269851175bdf769feab
zDreamHackz <zdreamhackzyt@gmail.com>
2016-08-28 22:38:43 +0200

    Create SubCommand.java

---
+++ src/partyundfreunde/utilities/SubCommand.java
@@ -0,0 +1,51 @@
+package de.SoonMitte.partyandfriends.utilities;
+
+import de.SoonMitte.partyandfriends.api.pafplayers.OnlinePAFPlayer;
+import de.SoonMitte.partyandfriends.main.Main;
+import net.md_5.bungee.api.chat.TextComponent;
+
+import java.util.ArrayList;
+
+public abstract class SubCommand implements Comparable<SubCommand> {
+	private final ArrayList<String> commands = new ArrayList<>();
+	public final TextComponent HELP;
+	private final int PRIORITY;
+	protected final String PREFIX;
+
+	protected SubCommand(String[] pCommands, int pPriority, TextComponent pHelp, String pPrefix) {
+		HELP = pHelp;
+		PRIORITY = pPriority;
+		PREFIX = pPrefix;
+		for (String command : pCommands)
+			commands.add(command.toLowerCase());
+	}
+
+	protected SubCommand(String[] pCommands, int pPriority, String pHelp, String pPrefix) {
+		HELP = new TextComponent(pHelp);
+		PRIORITY = pPriority;
+		PREFIX = pPrefix;
+		for (String command : pCommands)
+			commands.add(command.toLowerCase());
+	}
+
+	public boolean isApplicable(String pCommand) {
+		return commands.contains(pCommand.toLowerCase());
+	}
+
+	protected void sendError(OnlinePAFPlayer pPlayer, TextComponent pMessage) {
+		pPlayer.sendMessage(pMessage);
+		pPlayer.sendMessage(HELP);
+	}
+
+	protected void sendError(OnlinePAFPlayer pPlayer, String pIdentifier) {
+		sendError(pPlayer, new TextComponent(PREFIX + Main.getInstance().getMessagesYml().getString(pIdentifier)));
+	}
+
+	public abstract void onCommand(OnlinePAFPlayer pPlayer, String[] args);
+
+
+	@Override
+	public int compareTo(SubCommand o) {
+		return ((Integer) PRIORITY).compareTo(o.PRIORITY);
+	}
+}
