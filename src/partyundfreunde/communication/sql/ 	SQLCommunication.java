4403fbdecac8afa3c17a5535a11b743d24e70a31
zDreamHackz <zdreamhackzyt@gmail.com>
2016-08-28 21:32:31 +0200

    Create  	SQLCommunication.java

---
+++ src/partyundfreunde/communication/sql/ 	SQLCommunication.java
@@ -0,0 +1,88 @@
+package de.SoonMitte.partyandfriends.communication.sql;
+
+import java.sql.*;
+
+/**
+ * @author SoonMitte
+ * @version 1.0.0 created on 12.06.16
+ */
+public abstract class SQLCommunication {
+	/**
+	 * The MySQL database
+	 */
+	protected final String database;
+	/**
+	 * The url of the SQL server
+	 */
+	private final String url;
+	private Connection connection;
+
+	protected SQLCommunication(String pDatabase, String pURL) {
+		this.database = pDatabase;
+		this.url = pURL;
+		connection = createConnection();
+	}
+
+	protected void close(ResultSet rs, Statement stmt) {
+		try {
+			if (rs != null)
+				rs.close();
+			if (stmt != null)
+				stmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void close(ResultSet rs, Statement stmt, PreparedStatement prepStmt) {
+		try {
+			if (rs != null)
+				rs.close();
+			if (stmt != null)
+				stmt.close();
+			if (prepStmt != null)
+				prepStmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected Connection getConnection() {
+		try {
+			if (connection != null && connection.isValid(6))
+				return connection;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return connection = createConnection();
+	}
+
+	protected void close(PreparedStatement pPrepStmt) {
+		try {
+			if (pPrepStmt != null)
+				pPrepStmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private Connection createConnection() {
+		try {
+			closeConnection();
+			Class.forName("com.mysql.jdbc.Driver");
+			return DriverManager.getConnection(url);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public void closeConnection() {
+		try {
+			if (connection != null)
+				connection.close();
+		} catch (SQLException ignored) {
+		}
+	}
+
+}
