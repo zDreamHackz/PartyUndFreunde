81b079c7805b955b1dd5cfb30792372eb8304666
zDreamHackz <zdreamhackzyt@gmail.com>
2016-08-28 21:06:37 +0200

    Create OnlinePAFPlayer.java

---
+++ src/partyundfreunde/api/pafplayers/OnlinePAFPlayer.java
@@ -0,0 +1,29 @@
+
+package de.SoonMitte.partyandfriends.api.pafplayers;
+
+import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.protocol.packet.Chat;
+
+public interface OnlinePAFPlayer extends PAFPlayer {
+
+	void createEntry();
+
+	void connect(ServerInfo pInfo);
+
+	ServerInfo getServer();
+
+	boolean isOnline();
+
+	int changeSettingsWorth(int pSettingsID);
+
+	void sendPacket(Chat chat);
+
+	void updatePlayerName();
+
+	/**
+	 * @return Returns the player if he is on this Bungeecord. If he is on
+	 * another Bungeecord (RedisBungee) it returns null.
+	 */
+	ProxiedPlayer getPlayer();
+}
