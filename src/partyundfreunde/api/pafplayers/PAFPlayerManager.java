c77401f5e4ad4708b5e4052540f63ded5b364187
zDreamHackz <zdreamhackzyt@gmail.com>
2016-08-28 21:09:43 +0200

    Create PAFPlayerManager.java

---
+++ src/partyundfreunde/api/pafplayers/PAFPlayerManager.java
@@ -0,0 +1,24 @@
+package de.SoonMitte.partyandfriends.api.pafplayers;
+
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+
+import java.util.UUID;
+
+public abstract class PAFPlayerManager {
+	private static PAFPlayerManager instance;
+
+	public PAFPlayerManager() {
+		instance = this;
+	}
+
+	public static PAFPlayerManager getInstance() {
+		return instance;
+	}
+
+	public abstract PAFPlayer getPlayer(String pPlayer);
+
+	public abstract OnlinePAFPlayer getPlayer(ProxiedPlayer pPlayer);
+
+	public abstract PAFPlayer getPlayer(UUID pPlayer);
+
+}
