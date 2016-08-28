19915cbd3c22da6c47a28259092fd330c29e0976
zDreamHackz <zdreamhackzyt@gmail.com>
2016-08-28 21:12:05 +0200

    Create PartyAPI.java

---
+++ src/partyundfreunde/api/party/PartyAPI.java
@@ -0,0 +1,29 @@
+package de.SoonMitte.partyandfriends.api.party;
+
+import de.SoonMitte.partyandfriends.main.Main;
+import de.SoonMitte.partyandfriends.api.pafplayers.OnlinePAFPlayer;
+
+/**
+ * The APIs for the party system of Party and Friends
+ *
+ * @author Simonsator
+ * @version 1.0.0
+ */
+public class PartyAPI {
+	public static final int LEADER_PERMISSION_HEIGHT = 2;
+	public static final int PARTY_MEMBER_PERMISSION_HEIGHT = 1;
+	public static final int NO_PARTY_PERMISSION_HEIGHT = 0;
+
+	/**
+	 * Returns the party, if the given player is in a party or null if he is not
+	 * in a party
+	 *
+	 * @param player The player
+	 * @return Returns the party, if the given player is in a party or null if
+	 * he is not in a party
+	 */
+	public static PlayerParty getParty(OnlinePAFPlayer player) {
+		return Main.getPartyManager().getParty(player);
+	}
+
+}
