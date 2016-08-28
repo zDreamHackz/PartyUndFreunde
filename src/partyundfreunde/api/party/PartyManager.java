466235cbbf43147408ee47821874057e2feb308f
zDreamHackz <zdreamhackzyt@gmail.com>
2016-08-28 21:14:02 +0200

    Create PartyManager.java

---
+++ src/partyundfreunde/api/party/PartyManager.java
@@ -0,0 +1,72 @@
+package de.SoonMitte.partyandfriends.api.party;
+
+import de.SoonMitte.partyandfriends.api.pafplayers.OnlinePAFPlayer;
+import de.SoonMitte.partyandfriends.utilities.disable.Deactivated;
+import de.SoonMitte.partyandfriends.utilities.disable.Disabler;
+
+import java.util.HashMap;
+
+/**
+ * This class manages the partys.
+ *
+ * @author Simonsator
+ * @version 1.0.1
+ */
+public abstract class PartyManager implements Deactivated {
+	private static PartyManager instance;
+
+	protected PartyManager() {
+		Disabler.getInstance().registerDeactivated(this);
+		instance = this;
+	}
+
+	public static PartyManager getInstance() {
+		return instance;
+	}
+
+	/**
+	 * Returns the party in which a player is
+	 *
+	 * @param player The player
+	 * @return Returns the party in which a player is or null if he isn't in a
+	 * party.
+	 */
+	public abstract PlayerParty getParty(OnlinePAFPlayer player);
+
+	/**
+	 * Creates a party if the player is not already in a party.
+	 *
+	 * @param player The player
+	 * @return Returns true if the party was created or false if not
+	 */
+	public abstract PlayerParty createParty(OnlinePAFPlayer player);
+
+	protected abstract void deleteAllParties();
+
+	/**
+	 * Deletes a party which is given
+	 *
+	 * @param party The party
+	 */
+	public abstract void deleteParty(PlayerParty party);
+
+	/**
+	 * Puts the player with the party in the {@link HashMap}
+	 *
+	 * @param player The Player
+	 * @param party  The Party
+	 */
+	public abstract void addPlayerToParty(OnlinePAFPlayer player, PlayerParty party);
+
+	/**
+	 * Removes the player party link from the {@link HashMap}
+	 *
+	 * @param player The Player
+	 */
+	public abstract void removePlayerFromParty(OnlinePAFPlayer player);
+
+	@Override
+	public void onDisable() {
+		deleteAllParties();
+	}
+}
