0f189912dd02c08f5ad7f25084248539242979d0
zDreamHackz <zdreamhackzyt@gmail.com>
2016-08-28 21:22:08 +0200

    Create TopCommand.java

---
+++ src/partyundfreunde/api/TopCommand.java
@@ -0,0 +1,56 @@
+package de.SoonMitte.partyandfriends.api;
+
+import de.SoonMitte.partyandfriends.main.Main;
+import de.SoonMitte.partyandfriends.api.pafplayers.OnlinePAFPlayer;
+import de.SoonMitte.partyandfriends.utilities.Language;
+import de.SoonMitte.partyandfriends.utilities.SubCommand;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Command;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static de.SoonMitte.partyandfriends.main.Main.getPlayerManager;
+
+public abstract class TopCommand<T extends SubCommand> extends Command {
+	protected final ArrayList<T> subCommands = new ArrayList<>();
+
+	protected TopCommand(String[] pCommandNames, String pPermission) {
+		super(pCommandNames[0], pPermission, pCommandNames);
+	}
+
+	public static boolean isPlayer(CommandSender pCommandSender) {
+		if (!(pCommandSender instanceof ProxiedPlayer)) {
+			if (Main.getInstance().getLanguage() == Language.OWN) {
+				Main.getInstance().loadConfiguration();
+				pCommandSender.sendMessage(
+						new TextComponent(Main.getInstance().getFriendsPrefix() + "Config and MessagesYML reloaded!"));
+			} else {
+				Main.getInstance().loadConfiguration();
+				pCommandSender
+						.sendMessage(new TextComponent(Main.getInstance().getFriendsPrefix() + "Config reloaded"));
+			}
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public void execute(CommandSender pCommandSender, String[] args) {
+		if (isPlayer(pCommandSender))
+			onCommand(getPlayerManager().getPlayer((ProxiedPlayer) pCommandSender), args);
+	}
+
+	protected abstract void onCommand(OnlinePAFPlayer pPlayer, String args[]);
+
+	public void addCommand(T pCommand) {
+		subCommands.add(pCommand);
+		sort();
+	}
+
+	protected void sort() {
+		Collections.sort(subCommands);
+	}
+}
