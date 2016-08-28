369ff85c49731f2e0503fd3fe4137728d5e577bb
zDreamHackz <zdreamhackzyt@gmail.com>
2016-08-28 22:13:10 +0200

    Create LocalPartyManager.java

---
+++ src/partyundfreunde/party/partymanager/LocalPartyManager.java
@@ -0,0 +1,79 @@
+package de.SoonMitte.partyandfriends.party.partymanager;
+
+import de.SoonMitte.partyandfriends.api.pafplayers.OnlinePAFPlayer;
+import de.SoonMitte.partyandfriends.api.party.PartyManager;
+import de.SoonMitte.partyandfriends.party.playerpartys.LocalPlayerParty;
+import de.SoonMitte.partyandfriends.api.party.PlayerParty;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+
+public class LocalPartyManager extends PartyManager {
+	/**
+	 * All partys of players in a HashMap
+	 */
+	private static HashMap<UUID, PlayerParty> partys = new HashMap<>();
+
+	@Override
+	public PlayerParty getParty(OnlinePAFPlayer player) {
+		return partys.get(player.getUniqueId());
+	}
+
+	/**
+	 * Creates a party if the player is not already in a party.
+	 *
+	 * @param player The player
+	 * @return Returns true if the party was created or false if not
+	 */
+	@Override
+	public PlayerParty createParty(OnlinePAFPlayer player) {
+		PlayerParty party = new LocalPlayerParty(player);
+		partys.put(player.getUniqueId(), party);
+		return party;
+	}
+
+	@Override
+	public void deleteAllParties() {
+		partys = new HashMap<>();
+	}
+
+	/**
+	 * Deletes a party which is given
+	 *
+	 * @param party The party
+	 */
+	@Override
+	public void deleteParty(PlayerParty party) {
+		if (party != null) {
+			for (int i = 0; i < party.getPlayers().size(); i++) {
+				if (party.getPlayers().get(i) != null) {
+					partys.remove(party.getPlayers().get(i).getUniqueId());
+				}
+			}
+			if (party.getLeader() != null)
+				partys.remove(party.getLeader().getUniqueId());
+		}
+	}
+
+	/**
+	 * Puts the player with the party in the {@link HashMap}
+	 *
+	 * @param player The Player
+	 * @param party  The Party
+	 */
+	@Override
+	public void addPlayerToParty(OnlinePAFPlayer player, PlayerParty party) {
+		partys.put(player.getUniqueId(), party);
+	}
+
+	/**
+	 * Removes the player party link from the {@link HashMap}
+	 *
+	 * @param player The Player
+	 */
+	@Override
+	public void removePlayerFromParty(OnlinePAFPlayer player) {
+		partys.remove(player.getUniqueId());
+	}
+}
